6d8d11a6e817efde81cf8a6c31dbdcaf8d493185
carson-evans <carsonevans@outlook.com>
2023-12-07 11:10:54 -0500

    Email button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,10 +5,8 @@ import Container from "react-bootstrap/Container";
 import logo from "../Assets/logo.png";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
-import { CgGitFork } from "react-icons/cg";
-import { ImBlog } from "react-icons/im";
+import { AiFillMail } from "react-icons/ai";
 import {
-  AiFillStar,
   AiOutlineHome,
   AiOutlineFundProjectionScreen,
   AiOutlineUser,
@@ -91,14 +89,13 @@ function NavBar() {
                 <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
               </Nav.Link>
             </Nav.Item>
-            <Nav.Item className="fork-btn">
+            <Nav.Item className="email-btn">
               <Button
-                href="https://github.com/carson-evans/Portfolio"
+                href="mailto:CarsonEvans@Outlook.com"
                 target="_blank"
-                className="fork-btn-inner"
+                className="email-btn-inner"
               >
-                <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
-                <AiFillStar style={{ fontSize: "1.1em" }} />
+                <AiFillMail style={{ fontSize: "1.2em" }} />
               </Button>
             </Nav.Item>
           </Nav>
