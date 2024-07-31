35135204ce86648d33ba6ce016e9ad570abafda9
carson-evans <carsonevans@outlook.com>
2024-07-31 17:13:55 -0400

    FU

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -11,24 +11,11 @@ function AboutCard() {
             Hello everyone, my name is <span className="purple">Carson Evans, </span>
             and I'm from <span className="purple"> Dallas, Texas.</span>
             <br />
-            I am currently <span className="purple">seeking an internship in the Boston area.</span>
-            <br />
-            I am a <span className="purple">sophomore </span> studying <span className="purple">Software Engineering at Fisher College.</span>
+            I am a <span className="purple">junior </span> studying <span className="purple">Computer Science at UMass Boston.</span>
             <br />
             <br />
-            In addition to coding, here are some other activities I enjoy:
           </p>
-          <ul>
-            <li className="about-activity">
-              <ImPointRight /> Playing the Guitar
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Following UGA Football
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Watching Movies
-            </li>
-          </ul>
+
 
         </blockquote>
       </Card.Body>
--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -17,11 +17,11 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I am currently a <b className="purple">sophomore</b> at <b className="purple">Fisher College,</b> pursuing a <b className="purple">Bachelor's degree in Software Engineering</b>. I am passionate about learning new technologies, and developing software that addresses real-world problems.
+              I am currently a <b className="purple">Junior</b> at <b className="purple">UMass Boston,</b> pursuing a <b className="purple">Bachelor's degree in Software Engineering</b>. I am passionate about learning new technologies, and developing software that addresses real-world problems.
               <br />
               <br />I like to code in
               <i>
-                <b className="purple"> Python, JavaScript, Java, C#, C++, and HTML. </b>
+                <b className="purple"> Python, JavaScript, C#, C++, and Java. </b>
               </i>
               <br />
               <br />
