0f8998405cd8ff017569aeca7d75bce71c65a50b
carson-evans <carsonevans@outlook.com>
2025-10-27 15:40:23 -0400

    Fix: remove duplicat import. Upgrade: Node.js to 22.x

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/RESUME.pdf";
 import pdf from "../../Assets/../Assets/Carson_A._Evans_RESUME.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
